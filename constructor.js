17246143b890654fa622c9c35eee827bfa21fd1e
LunaMoonfangs <nicu.sighencea@yahoo.com>
2022-02-10 01:46:44 +0200

    add accumulator

--- constructor.js
+++ constructor.js
@@ -43,8 +43,11 @@ let calculator = {
   calculator.read();
   alert( calculator.sum() );
   alert( calculator.mul() );
+
   // *************************************
-*/
+
+  */
+/*
 function Calculator (){
 
     this.nr1 = 0 ;
@@ -71,3 +74,49 @@ calculator.read();
 
   alert( `Suma dintre ${calculator.nr1} si ${calculator.nr2} este ` + calculator.sum() );
   alert( `Produsul dintre ${calculator.nr1} si ${calculator.nr2} este ` + calculator.mul() );
+
+*/
+
+// ****************************************************
+
+/*
+Create new Accumulator
+importance: 5
+
+Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+    Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+    The read() method should use prompt to read a new number and add it to value.
+
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+Here’s the demo of the code:
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
+
+*/
+
+function Accumulator (startingValue){
+
+  this.value = startingValue;
+
+  this.read = function () {
+    this.value += Number( prompt(`Suma este ${this.value}, mai adaugi ? `, this.startingValue) );
+  }
+  
+}
+
+let accumulator = new Accumulator(1);
+
+accumulator.read();
+accumulator.read();
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
