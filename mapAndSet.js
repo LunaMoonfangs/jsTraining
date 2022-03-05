c2198ba0c7743e77275fffba4d76ec5e2871574b
LunaMoonfangs <nicu.sighencea@yahoo.com>
2022-03-05 16:53:16 +0200

    task 1 de la map si set

---
+++ mapAndSet.js
@@ -0,0 +1,131 @@
+// ***************** 04.03.22**********
+
+
+/*
+Filter unique array members
+importance: 5
+
+Let arr be an array.
+
+Create a function unique(arr) that should return an array with unique items of arr.
+
+For instance:
+
+function unique(arr) {
+  // your code
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(values) ); // Hare, Krishna, :-O
+
+P.S. Here strings are used, but can be values of any type.
+
+P.P.S. Use Set to store unique values.
+
+Open a sandbox with tests.
+
+*/
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+function uniqueArr(arr) {
+    
+    // return Array.from(new Set(arr));
+    let unique = new Set();
+
+    for(let elm of arr){
+        unique.add(elm);
+    }
+
+    unique.forEach( (value) => { 
+        console.log(`value ${value}`);
+        return  value; 
+    });
+
+    return Array.from(unique); 
+    //nu afiseaza unique :( 
+    
+}
+
+console.log(`Set arr: `);
+console.log(uniqueArr(values) );
+
+// **************************
+
+/*
+
+Filter anagrams
+importance: 4
+
+Anagrams are words that have the same number of same letters, but in different order.
+
+For instance:
+
+nap - pan
+ear - are - era
+cheaters - hectares - teachers
+
+Write a function aclean(arr) that returns an array cleaned from anagrams.
+
+For instance:
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+
+From every anagram group should remain only one word, no matter which one.
+
+*/
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+
+    // despart cuvintele in array de litere [a,c,b,d]
+
+    let tempArr = [];
+    let set = new Set();
+    let map = new Map();
+
+    for (let char of arr){
+        tempArr.push(char.toLowerCase().split("").sort().join(""));
+    }
+    console.log(`TempArr: `)
+    console.log(tempArr); // ok [ [] , [] , [] ] 
+
+    // sortez fiecare array [ [a,b,c,d] , arr.lenght] ]  // am trecut direct .sort() in linia 95
+    
+    // trebuie bagat un toLowerCase sau toUpperCase ca sa le egaleze .. error toLowerCase() is not a funciton
+
+
+    // le introduc intr-un map cu pereche de [ [cuvant sortat] , [cuvant normal] ]
+    
+    //map.entries() – returns an iterable for entries [key, value], it’s used by default in for..of
+
+    for(let elm of tempArr){
+        set.add(elm);
+        map.entries([elm, findIndex(elm)]);
+            
+    }
+
+    set.forEach( (value) => { 
+        console.log(`value pt set este: ${value}`);
+        return  value; 
+    });
+
+    
+    let setArray = Array.from(set); // le ia pe toate :(
+
+    //set.has(value) – returns true if the value exists in the set, otherwise false
+
+    
+    console.log(`Array fara anagrame este: ` + setArray);
+}
+
+
+aclean(arr);
